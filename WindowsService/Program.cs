bb1349735f83badbd019c2cb8cb70dfe673a282a
Glen Hallworth <>
2018-07-02 13:53:36 +1000

    Add project files.

---
+++ WindowsService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
